ce287104858732b4da2097fef454b790edccaeef
benedictquartey <hackgh94@gmail.com>
2017-01-21 21:34:04 +0000

    Add files via upload

---
+++ LabCalculator.java
@@ -0,0 +1,28 @@
+
+
+public class LabCalculator{
+	
+	public static void main(String args[]){
+
+		if(args[0].equalsIgnoreCase("add")){
+			 System.out.println(Double.parseDouble(args[1])+Double.parseDouble(args[2]));
+		}
+
+		if(args[0].equalsIgnoreCase("subtract")){
+			 System.out.println(Double.parseDouble(args[1])-Double.parseDouble(args[2]));
+		}
+
+		if(args[0].equalsIgnoreCase("multiply")){
+			 System.out.println(Double.parseDouble(args[1])*Double.parseDouble(args[2]));
+		}
+
+		
+
+
+
+		
+
+	}
+
+	
+}
